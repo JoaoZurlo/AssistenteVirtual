3d08753703b9fca5de0c99635bb4fbc868a5e6ee
Joao Paulo Zurlo <j.zurlo@intertox.com.br>
2024-03-01 19:57:12 -0300

    atualizando

---
+++ Assistente.py
@@ -0,0 +1,28 @@
+import pyttsx3
+import speech_recognition as sr
+from playsound import playsound
+import random
+import datetime
+hour = datetime.datetime.now().strftime('%H:%M')
+print(hour)
+date = datetime.date.today().strftime('%d/%B/%y')
+print(date)
+date = date.split('/')
+print(date)
+
+import webbrowser as wb
+import tensorflow as tf
+import numpy as np
+import librosa
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+
+
+
+
+
+
+
+
---
+++ Modules/comandos_respostas.py
@@ -0,0 +1,29 @@
+#Comandos
+
+funcoes = ['o que você pode fazer', 'o que você faz', 'funcionalidades', 'o que você sabe fazer', 'o que mais você sabe fazer']
+lembretes = ['anotação', 'anotar', 'nova anotação', 'novo lembrete' 'lembrar', 'lembrete', 'anote', 'mais uma anotação', 'anotar novamente', 'anote novamente', 'nota']
+ajuda = ['pesquisar', 'preciso de ajuda', 'somente ajuda', 'pode me ajudar', 'estou com uma dúvida', 'tenho uma pergunta', 'tenho uma dúvida']
+horas = ['que horas são', 'hora', 'hora agora', 'que horas são agora', 'qual é a hora']
+data = ['que dia é hoje', 'que dia é']
+analysis_mode = ['modo de emoção', 'ativar emoção']
+agenda = ['eventos', 'agenda hoje', 'agenda', 'compromissos hoje', 'eventos de hoje', 'compromissos de hoje', 'eventos de hoje']
+
+comandos = [funcoes, lembretes, ajuda, horas, data, analysis_mode, agenda ]
+#print(comandos)
+
+
+
+#Respostas
+funcionalidades = 'Gravar lembretes, fazer pesquisa no google, falar as horas, falar a dta, falar eventos agendados para o dia. Também tenho o modo de analise que, quando ativado, faço a anáçise da sua emoção baseado na sua voz'
+respostas_conclusão = ['Ok!', 'Concluído!', 'Feito!', 'Tudo certo', 'Terminado!', 'Perfeito!', 'Realizado!']
+perguntas = ['Como possoajudar?', 'Ok, vamos lá', 'Certo, é só falar!']
+respostas_agradecimentos = ['Se precisar é só chamar', 'Qualquer coisa estou aqui']
+despedida = ['Até mais', 'Até breve', 'Até a próxima']
+
+respostas = [funcionalidades, respostas_conclusão, perguntas, respostas_agradecimentos, despedida]
+#print(respostas)
+
+
+
+
+
