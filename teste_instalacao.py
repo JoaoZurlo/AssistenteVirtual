0b6d213d19c9ff7ebd7b4b64f6ee3eb15f94d677
Joao Paulo Zurlo <j.zurlo@intertox.com.br>
2024-02-29 21:01:35 -0300

    The first commit

---
+++ teste_instalacao.py
@@ -0,0 +1,11 @@
+#import pygame
+#pygame.init()
+#pygame.mixer.music.load('n3.mp3')
+#pygame.mixer.music.play()
+#pygame.time.delay(5000)
+
+#import speech_recognition
+#print('Speech Recognition', speech_recognition.__version__)
+
+import pyttsx3
+pyttsx3.speak('Testando a biblioteca')
