1700e4460b35b47026f7c3449e4a0be28467fc11
Joao Paulo Zurlo <j.zurlo@intertox.com.br>
2024-02-29 21:52:27 -0300

    Uploading changes to the project

--- teste_instalacao.py
+++ teste_instalacao.py
@@ -7,5 +7,20 @@
 #import speech_recognition
 #print('Speech Recognition', speech_recognition.__version__)
 
-import pyttsx3
-pyttsx3.speak('Testando a biblioteca')
+#import pyttsx3
+#pyttsx3.speak('Testando a biblioteca')
+
+#import tensorflow as tf
+
+#print('TensorFlow: ', tf.__version__)
+
+#import librosa
+#print('Liborsa', librosa.__version__)
+
+#import matplotlib
+#print('Matplotlib', matplotlib.__version__)
+#import seaborn
+#print('Seaborn', seaborn.__version__)
+
+import pyaudio
+print('Pyaudio')
