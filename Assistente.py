f1f9bd24b1382f12f7ea1df1eb75412849fec7e6
Joao Paulo Zurlo <j.zurlo@intertox.com.br>
2024-03-03 21:07:47 -0300

    Alterações e implemnetações novas

--- Assistente.py
+++ Assistente.py
@@ -33,3 +33,32 @@ def search(frase):
 
 #search('linguagem python')
 
+MODEL_TYPES = ['EMOÇÃO']
+
+def load_model_by_name(model_type):
+    if model_type == MODEL_TYPES[0]:
+        model = tf.keras.models.load_model('models/speech_emotion_recognition.hdf5')
+        model_dict = sorted(list(['neutra', 'calma', 'feliz', 'triste', 'nervosa', 'medo', 'nojo', 'surpreso']))
+        SAMPLE_RATE = 48000
+    return model, model_dict, SAMPLE_RATE
+
+#print(load_model_by_name('EMOÇÃO'))
+#print(load_model_by_name('EMOÇÃO')[0].summary())
+
+model_type = 'EMOÇÃO'
+loaded_model = load_model_by_name(model_type)
+
+def predict_sound(AUDIO, SAMPLE_PATE, plot = True):
+    results = []
+    wav_data, sample_rate = librosa.load(AUDIO, sr=SAMPLE_PATE)
+    #print(wav_data)
+    #print(wav_data.shape)
+    clip, index = librosa.effects.trim(wav_data, top_db=60, frame_length=512, hop_length=64)
+    splitted_audio_data = tf.signal.frame(clip, sample_rate, sample_rate, pad_end=True, pad_value=0)
+    for i, data in enumerate(splitted_audio_data.numpy()):
+        print('Audio')
+
+
+
+
+predict_sound('triste.wav', loaded_model[2], plot=True)
