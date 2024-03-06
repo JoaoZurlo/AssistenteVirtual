11b80fee2a8fdd192c45a1a81c7475c29f786668
Joao Paulo Zurlo <j.zurlo@intertox.com.br>
2024-03-05 22:28:52 -0300

    Projeto ja reconhecendo emoções com auidos enviados

--- Assistente.py
+++ Assistente.py
@@ -1,8 +1,7 @@
-import pyttsx3
-import speech_recognition as sr
-from playsound import playsound
-import random
 import datetime
+
+import matplotlib.pyplot as plt
+
 hour = datetime.datetime.now().strftime('%H:%M')
 print(hour)
 date = datetime.date.today().strftime('%d/%B/%y')
@@ -14,68 +13,80 @@ import webbrowser as wb
 import tensorflow as tf
 import numpy as np
 import librosa
-import matplotlib.pyplot as plt
 import seaborn as sns
+
 sns.set()
 from modules import comandos_respostas
+
 comandos = comandos_respostas.comandos
 resposta = comandos_respostas.respostas
-#print(comandos)
-#print(resposta)
+# print(comandos)
+# print(resposta)
 
 meu_nome = 'TechZurlo'
 
 chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
 
+
 def search(frase):
     wb.get(chrome_path).open('https://www.google.com/search?q=' + frase)
 
 
-#search('linguagem python')
+# search('linguagem python')
 
 MODEL_TYPES = ['EMOÇÃO']
 
+
 def load_model_by_name(model_type):
+    global model, model_dict, SAMPLE_RATE
     if model_type == MODEL_TYPES[0]:
         model = tf.keras.models.load_model('models/speech_emotion_recognition.hdf5')
         model_dict = sorted(list(['neutra', 'calma', 'feliz', 'triste', 'nervosa', 'medo', 'nojo', 'surpreso']))
         SAMPLE_RATE = 48000
     return model, model_dict, SAMPLE_RATE
 
-#print(load_model_by_name('EMOÇÃO'))
-#print(load_model_by_name('EMOÇÃO')[0].summary())
+
+# print(load_model_by_name('EMOÇÃO'))
+# print(load_model_by_name('EMOÇÃO')[0].summary())
 
 model_type = 'EMOÇÃO'
 loaded_model = load_model_by_name(model_type)
 
-def predict_sound(AUDIO, SAMPLE_RATE, plot = True):
+
+def predict_sound(AUDIO, SAMPLE_RATE, plot=True):
     results = []
     wav_data, sample_rate = librosa.load(AUDIO, sr=SAMPLE_RATE)
-    #print(wav_data)
-    #print(wav_data.shape)
+    # print(wav_data)
+    # print(wav_data.shape)
     clip, index = librosa.effects.trim(wav_data, top_db=60, frame_length=512, hop_length=64)
     splitted_audio_data = tf.signal.frame(clip, sample_rate, sample_rate, pad_end=True, pad_value=0)
     for i, data in enumerate(splitted_audio_data.numpy()):
-        #print('Audio split: ', i)
-        #print(data)
-        #print(data.shape)
-        mfccs_features = librosa.feature.mfcc(y = data, sr = sample_rate, n_mfcc=48)
-        #print(mfccs_features.shape)
-        #print(mfccs_features)
-        mfccs_scalad_features = np.mean(mfccs_features.T, axis = 0)
+        # print('Audio split: ', i)
+        # print(data)
+        # print(data.shape)
+        mfccs_features = librosa.feature.mfcc(y=data, sr=sample_rate, n_mfcc=48)
+        # print(mfccs_features.shape)
+        # print(mfccs_features)
+        mfccs_scalad_features = np.mean(mfccs_features.T, axis=0)
         mfccs_scalad_features = mfccs_scalad_features.reshape(1, -1)
-        #print(mfccs_scalad_features.shape)
-        mfccs_scalad_features = mfccs_scalad_features[:,:,np.newaxis]
-        #print(mfccs_scalad_features.shape)
+        # print(mfccs_scalad_features.shape)
+        mfccs_scalad_features = mfccs_scalad_features[:, :, np.newaxis]
+        # print(mfccs_scalad_features.shape)
 
         predictions = loaded_model[0].predict(mfccs_scalad_features, batch_size=32)
         print(predictions)
-        print(predictions.sum())
+        #print(predictions.sum())
+        if plot:
+            plt.figure(figsize=(len(splitted_audio_data), 5))
+            plt.barh(loaded_model[1], predictions[0])
+            plt.tight_layout()
+            plt.show()
+
+        predictions = predictions.argmax(axis= 1)
+        print(predictions)
+        predictions = predictions.astype(int).flatten()
+        predictions = loaded_model[1][predictions[0]]
+        results.append(predictions)
+        print(results)
 
 predict_sound('triste.wav', loaded_model[2], plot=True)
-
-
-
-
-
-
